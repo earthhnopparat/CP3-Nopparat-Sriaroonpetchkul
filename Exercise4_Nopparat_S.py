72960a3758cc074be69b3313810dfd268da5e29b
earthhnopparat <110083922+earthhnopparat@users.noreply.github.com>
2022-07-28 11:11:43 +0700

    Exercise4_Nopparat_S.py

---
+++ Exercise4_Nopparat_S.py
@@ -0,0 +1,9 @@
+FE = 85.25
+GB = 75.75
+ICS = 68.8
+CP = 50.25
+print("--- Your Score ---")
+print("Foundation English :", FE)
+print("General Business :", GB)
+print("Introduction to Computer Systems :", ICS)
+print("Computer Programming :", CP)
