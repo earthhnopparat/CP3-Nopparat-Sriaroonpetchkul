64700fc00ce7219a1ba37b91268dbcaa32b89a01
earthhnopparat <110083922+earthhnopparat@users.noreply.github.com>
2022-07-29 10:34:42 +0700

    Add files via upload

---
+++ Exercise5_1_Nopparat_S.py
@@ -0,0 +1,6 @@
+a = int(input("ตัวเลขแรก : "))
+b = int(input("ตัวเลขสอง : "))
+print("a + b เท่ากับ", a+b )
+print("a - b เท่ากับ", a-b )
+print("a * b เท่ากับ", a*b )
+print("a / b เท่ากับ", a/b )
